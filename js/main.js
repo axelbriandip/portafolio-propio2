0c0f6dd0a35d23b22c02149bdf0c4c67944c7fc1
axelbriandip <axelbriandip.rg@gmail.com>
2022-06-02 03:38:46 -0300

    nav de mobile hecho (falta el de desktop)

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let iconoMenu = document.querySelector(".icono-menu");
+let navToggle = document.querySelector(".nav-toggle");
+
+iconoMenu.addEventListener("click", function(){
+    navToggle.classList.toggle("toggle");
+});
