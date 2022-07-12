e2d0758f347c65fe5336f4fbe93a527e520c275e
axelbriandip <axelbriandip.rg@gmail.com>
2022-07-12 16:33:26 -0300

    agregué dark mode

--- js/main.js
+++ js/main.js
@@ -4,3 +4,31 @@ let navToggle = document.querySelector(".nav-toggle");
 iconoMenu.addEventListener("click", function(){
     navToggle.classList.toggle("toggle");
 });
+
+const moon = document.querySelector(".moon");
+const moon2 = document.querySelector(".moon2");
+const body = document.querySelector("body");
+
+load();
+
+moon.addEventListener("click", function(){
+    body.classList.toggle("darkmode");
+    store(body.classList.contains("darkmode"));
+});
+moon2.addEventListener("click", function(){
+    body.classList.toggle("darkmode");
+    store(body.classList.contains("darkmode"));
+});
+
+function load() {
+    const darkmode = localStorage.getItem('darkmode');
+    if(!darkmode) {
+        store('false');
+    } else if(darkmode == 'true') {
+        body.classList.add("darkmode");
+    }
+}
+
+function store(value) {
+    localStorage.setItem('darkmode', value)
+}
