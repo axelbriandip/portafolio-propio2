0df2202633762e6ec3875b7b7f8dd50d7a1ef211
axelbriandip <axelbriandip.rg@gmail.com>
2022-07-12 18:49:27 -0300

    agregué ocultar nav desplegable en mobile cuando se elija sección

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// show nav-mobile
 let iconoMenu = document.querySelector(".icono-menu");
 let navToggle = document.querySelector(".nav-toggle");
 
@@ -5,6 +6,33 @@ iconoMenu.addEventListener("click", function(){
     navToggle.classList.toggle("toggle");
 });
 
+// hidden nav-mobile when to click in the item
+const toHome = document.querySelector(".to-home");
+toHome.addEventListener("click", function() {
+    navToggle.classList.toggle("toggle");
+})
+const toacercademi = document.querySelector(".to-acercademi");
+toacercademi.addEventListener("click", function() {
+    navToggle.classList.toggle("toggle");
+})
+const toMishabilidades = document.querySelector(".to-mishabilidades");
+toMishabilidades.addEventListener("click", function() {
+    navToggle.classList.toggle("toggle");
+})
+const toExperiencia = document.querySelector(".to-experiencia");
+toExperiencia.addEventListener("click", function() {
+    navToggle.classList.toggle("toggle");
+})
+const toPortafolio = document.querySelector(".to-portafolio");
+toPortafolio.addEventListener("click", function() {
+    navToggle.classList.toggle("toggle");
+})
+const toContacto = document.querySelector(".to-contacto");
+toContacto.addEventListener("click", function() {
+    navToggle.classList.toggle("toggle");
+})
+
+// change theme
 const moon = document.querySelector(".moon");
 const moon2 = document.querySelector(".moon2");
 const body = document.querySelector("body");
